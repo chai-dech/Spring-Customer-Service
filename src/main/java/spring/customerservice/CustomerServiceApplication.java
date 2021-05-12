9a967240789d993c82948dcf0e002a00eef984f4
chaitradech <kullachandachaitra@gmail.com>
2021-05-13 01:55:13 +0530

    Feat/added model class and controller

--- src/main/java/spring/customerservice/CustomerServiceApplication.java
+++ src/main/java/spring/customerservice/CustomerServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("added for testing");
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
