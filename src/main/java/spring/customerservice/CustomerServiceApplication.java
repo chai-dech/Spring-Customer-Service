b936190bfffb05b2a0874a8d7944b32604c09058
chaitradech <kullachandachaitra@gmail.com>
2021-05-12 23:08:10 +0530

    for testing

--- src/main/java/spring/customerservice/CustomerServiceApplication.java
+++ src/main/java/spring/customerservice/CustomerServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("my spring application");
+		System.out.println("added for testing");
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
