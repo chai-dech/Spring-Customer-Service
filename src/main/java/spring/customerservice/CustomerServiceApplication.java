08121e7a235167a99665019d48d6d6d51937b57f
chaitradech <kullachandachaitra@gmail.com>
2021-05-12 21:53:07 +0530

    check commit

--- src/main/java/spring/customerservice/CustomerServiceApplication.java
+++ src/main/java/spring/customerservice/CustomerServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("my spring application");
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
+
 
 }
